d3463e6cbcf8cc0ae7015cb9bd3f54cef42127a2
Casey Warren <caseywar1017@gmail.com>
2021-01-12 17:27:50 -0500

    dom elements grabbed, lets established, compareNumbers function written

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-// import functions and grab DOM elements
+import { compareNumbers } from './utils.js';
+
+const userInput = document.getElementById("user-input");
+const guessButton = document.getElementById("guess-button");
+const remainingGuesses = document.getElementById("remaining-guesses");
+const feedbackSpace = document.getElementById("feedback-space");
+// console.log(userInput, guessButton, remainingGuesses, feedbackSpace);
 
 // initialize state
+let randomNumber = Math.ceil(Math.random() * 20);
+let guessesRemainingLet = 4;
+// console.log(guessesRemainingLet, randomNumber);
+
+guessButton.addEventListener('click', () => {
+    // console.log('sup');
+    guessesRemainingLet--;
+
+
+});
+
 
 // set event listeners to update state and DOM
